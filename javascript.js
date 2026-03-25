06b1fec68c80581c9aa524ca4f0ff7fa3e871284
Ewura-bena685 <appiahewurabena685@gmail.com>
2026-03-25 20:46:59 +0000

    learning about data types in Javascript

---
+++ javascript.js
@@ -0,0 +1,30 @@
+ let name = "Ewurabena";
+console.log(name);
+
+ let age = 18;
+ console.log(age);
+
+let addition = 11 + 4;
+console.log(addition);
+
+let subtraction = 30 - 10;
+console.log(subtraction);
+
+let division = 50 / 5;
+console.log(division);
+
+let product = 6 * 7;
+console.log(product);
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+let bool1 = true;
+console.log(bool1);
+
+let num = 1000;
+console.log(typeof num);
+
+
+let text = "Learning JavaScript, kinda nervous.";
+console.log(typeof text);
