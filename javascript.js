2c202599b9e897716b83dd088cc17d4b9d7aee28
Ewura-bena685 <appiahewurabena685@gmail.com>
2026-03-27 12:39:39 +0000

    Conditions in Javascript

--- javascript.js
+++ javascript.js
@@ -1,30 +1,59 @@
- let name = "Ewurabena";
-console.log(name);
+// let names = "Ewurabena";
+//console.log(name);
 
- let age = 18;
- console.log(age);
+ //let ages = 18;
+ //console.log(age);
 
-let addition = 11 + 4;
-console.log(addition);
+//let addition = 11 + 4;
+//console.log(addition);
 
-let subtraction = 30 - 10;
-console.log(subtraction);
+//let subtraction = 30 - 10;
+//console.log(subtraction);
 
-let division = 50 / 5;
-console.log(division);
+//let division = 50 / 5;
+//console.log(division);
 
-let product = 6 * 7;
-console.log(product);
+//let product = 6 * 7;
+//console.log(product);
 
-let remainder = 15 % 4;
-console.log(remainder);
+//let remainder = 15 % 4;
+//console.log(remainder);
 
-let bool1 = true;
-console.log(bool1);
+//let bool1 = true;
+//console.log(bool1);
 
-let num = 1000;
-console.log(typeof num);
+//let num = 1000;
+//console.log(typeof num);
 
 
-let text = "Learning JavaScript, kinda nervous.";
-console.log(typeof text);
+//let text = "Learning JavaScript, kinda nervous.";
+//console.log(typeof text);
+
+// Prompt the user to enter a number
+//check if it is even or odd
+//display that the number the user entered is even or odd
+//let number = prompt("Enter a number")
+//if( number % 2 == 0){
+   // alert(`${number} is even`)
+//}else{
+   // alert(`${number} is odd`)
+//}
+
+//Take user's name
+// Ask for age
+//if they are 18 and above
+// if they are less than 18,
+
+let name =prompt('What is your name?')
+let age = prompt('How old are you?')
+age = Number(age);
+let email = prompt(" Please enter your email")
+if (age < 12) {
+    alert(`Hi ${name}. You are ${age} years old and you are too young to register.`)
+}else if (age >= 12 && age <= 17){
+    alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email}`)
+}
+else if (age >= 18) {
+    alert(`Hi ${name}. You are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email}`)
+}
+
