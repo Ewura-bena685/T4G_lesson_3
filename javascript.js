34dc62b64ba9c613ab37634dee6b254ad20128b3
Ewura-bena685 <appiahewurabena685@gmail.com>
2026-03-31 12:16:17 +0000

    learning about functions in js

--- javascript.js
+++ javascript.js
@@ -44,16 +44,29 @@
 //if they are 18 and above
 // if they are less than 18,
 
-let name =prompt('What is your name?')
-let age = prompt('How old are you?')
-age = Number(age);
-let email = prompt(" Please enter your email")
-if (age < 12) {
-    alert(`Hi ${name}. You are ${age} years old and you are too young to register.`)
-}else if (age >= 12 && age <= 17){
-    alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email}`)
-}
-else if (age >= 18) {
-    alert(`Hi ${name}. You are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email}`)
-}
+//let name =prompt('What is your name?')
+//let age = prompt('How old are you?')
+//age = Number(age);
+//let email = prompt(" Please enter your email")
+//if (age < 12) {
+    //alert(`Hi ${name}. You are ${age} years old and you are too young to register.`)
+//}else if (age >= 12 && age <= 17){
+    //alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email}`)
+//}
+//else if (age >= 18) {
+    //alert(`Hi ${name}. You are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email}`)
+//}
+
+function checkGender(){ //creating a function to check gender.
+    let gender = prompt('What is your gender? Type "M" for Male, "F" for Female '); //asking the user to input their gender. and store in the variable "gender".
+    if (gender == 'M' || gender == 'm' || gender == 'Male' || gender == 'male'){
+        alert("You are a boy👦🏾");}
+    else if (gender == 'F' || gender == 'f' || gender == 'Female' || gender == 'female'){
+            alert("You are a girl👧🏾");}
+    else{
+        alert("Please ask your parents or an adult for help.")
+    }
+
+    }
+    checkGender();
 
